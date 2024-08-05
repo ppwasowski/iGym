b0597221e146aa61139e42d1f73b03d72112c591
p.p.wasowski <p.p.wasowski@gmail.com>
2024-08-05 21:01:52 +0200

    pressable fix

--- components/Pressable.js
+++ components/Pressable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Pressable, Text } from 'react-native';
 import { styled } from 'nativewind';
 
-const StyledPressable = styled(Pressable, 'bg-Primary h-24 w-44 m-1 mt-2 rounded-lg justify-center items-center shadow-lg');
+const StyledPressable = styled(Pressable, 'bg-Primary h-24 w-40 m-1 mt-2 rounded-lg justify-center items-center shadow-lg');
 const StyledText = styled(Text, 'text-Text text-lg font-bold capitalize');
 
 const CustomPressable = ({ onPress, title, textStyle, ...props}) => {
